529ca4c4e5de82165f6e56c614f2e0a354275884
Akshay Chorage <108932059+AkshayChorage@users.noreply.github.com>
2022-07-08 17:37:04 +0530

    Add files via upload

---
+++ Day01_Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 50;
+            count = count + 1;
+            Console.WriteLine(count);
+            Console.WriteLine("Welcome to C#.NET...");
+
+            Point point = new Point();
+            point.X = 25;
+            point.Y = 07;
+            Console.WriteLine(point);
+
+            DateTime birthDate1 = new DateTime(1997, 04, 25);
+            Person p1 = new Person("Akshay", "Chorage", birthDate1, "9881234567", "abc@gmail.com");
+            Console.WriteLine(p1);
+
+            Console.Read();
+        }
+    }
+}
